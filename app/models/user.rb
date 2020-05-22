7fb693e8b399dc676fab054f81ac23659a824b6b
irifune-tatsuya <irifune.tatsuya@gmail.com>
2020-05-22 11:32:35 +0900

    add "user login & registration", "address registration", "localizing into ja",  "models test"

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  has_one :address
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :nickname,                         presence: true, uniqueness: true
+  validates :password,                         length: { in: 7..128 }
+  validates :first_name, :last_name,           presence: true
+  validates :first_name_kana, :last_name_kana, presence: true,
+                                                format: {
+                                                with: /\A[ァ-ヶー－]+\z/,
+                                                message: "は全角カタカナで入力して下さい"
+                                                }
+end
