bba241c706581968926ce15535e5e155d0d3bba1
kyonkyonkyonkyon <k.kusano.8.19@gmail.com>
2020-05-28 20:53:22 +0900

    delete_items_migration_file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_24_051517) do
+ActiveRecord::Schema.define(version: 2020_05_24_051516) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "post_code", null: false
@@ -33,14 +33,6 @@ ActiveRecord::Schema.define(version: 2020_05_24_051517) do
     t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "item_name", null: false
-    t.bigint "category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_items_on_category_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", null: false
@@ -60,5 +52,4 @@ ActiveRecord::Schema.define(version: 2020_05_24_051517) do
   end
 
   add_foreign_key "addresses", "users"
-  add_foreign_key "items", "categories"
 end
